25cd843c0b5495106846bf52e80307a554160664
Aljoscha Naß <straffer_ueber_0v@icloud.com>
2025-03-03 13:47:34 +0100

    base file

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function init() {
+    render();
+}
+
+function render() {
+    templatePokemonCard();
+}
---
+++ scripts/templates.js
@@ -0,0 +1,15 @@
+function templatePokemonCard() {
+    return `
+        <div class="pokecard bg_grass">
+            <div id="pokecard_header" class="pokecard_header">
+                <h3>#1</h3>
+                <h3>Bulbasaur</h3>
+            </div>
+            <div id="pokecard_img" class="pokecard_img">
+                <img src="" alt="">
+            </div>
+            <div id="pokecard_footer" class="pokecard_footer">
+            </div>
+        </div>
+    `;
+}
