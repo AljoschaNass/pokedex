e0a88d544f9d4b0d3b617298cf9b4010e72362cc
Aljoscha Naß <straffer_ueber_0v@icloud.com>
2025-03-06 11:39:10 +0100

    overlay clean code - fetch 21 pokemons

--- script.js
+++ script.js
@@ -9,38 +9,41 @@ function render() {
 }
 
 async function fetchDataJson() {
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon/7");
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=21&offset=0");
     let responseAsJson = await response.json();
-    console.log(responseAsJson.name);
-    console.log(responseAsJson.id);
-    console.log(responseAsJson.sprites.other.dream_world.front_default);
-    console.log(responseAsJson.types[0].type.name);
+    //console.log(responseAsJson.name);
+    //console.log(responseAsJson.id);
+    //console.log(responseAsJson.sprites.other.dream_world.front_default);
+    //console.log(responseAsJson.types[0].type.name);
     //console.log(responseAsJson.types[1].type.name);
-    console.log(responseAsJson.stats[0].stat.name);
-    console.log(responseAsJson.stats[0].base_stat);
-    console.log(responseAsJson.stats[1].stat.name);
-    console.log(responseAsJson.stats[1].base_stat);
-    console.log(responseAsJson.stats[2].stat.name);
-    console.log(responseAsJson.stats[2].base_stat);
-    console.log(responseAsJson.stats[3].stat.name);
-    console.log(responseAsJson.stats[3].base_stat);
-    console.log(responseAsJson.stats[4].stat.name);
-    console.log(responseAsJson.stats[4].base_stat);
-    console.log(responseAsJson.stats[5].stat.name);
-    console.log(responseAsJson.stats[5].base_stat);
+    //console.log(responseAsJson.stats[0].stat.name);
+    //console.log(responseAsJson.stats[0].base_stat);
+    //console.log(responseAsJson.stats[1].stat.name);
+    //console.log(responseAsJson.stats[1].base_stat);
+    //console.log(responseAsJson.stats[2].stat.name);
+    //console.log(responseAsJson.stats[2].base_stat);
+    //console.log(responseAsJson.stats[3].stat.name);
+    //console.log(responseAsJson.stats[3].base_stat);
+    //console.log(responseAsJson.stats[4].stat.name);
+    //console.log(responseAsJson.stats[4].base_stat);
+    //console.log(responseAsJson.stats[5].stat.name);
+    //console.log(responseAsJson.stats[5].base_stat);
     console.log(responseAsJson);
+    console.log(responseAsJson.results[0]);
 }
 
 function openOverlay() {
     let overlayRef = document.getElementById("overlay");
     overlayRef.classList.remove("d_none");
     overlayRef.classList.add("d_flex");
+    togglePositionFixed();
 }
 
 function closeOverlay() {
     let overlayRef = document.getElementById("overlay");
     overlayRef.classList.add("d_none");
     overlayRef.classList.remove("d_flex");
+    togglePositionFixed();
 }
 
 function nextPicture(event) {
@@ -67,4 +70,9 @@ function previousPicture(event) {
 
 function stopEventBubbling(event) {
     event.stopPropagation();
+}
+
+function togglePositionFixed() {
+    let contentRef = document.getElementById("content");
+    contentRef.classList.toggle("position_fixed");
 }
